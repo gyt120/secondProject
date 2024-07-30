54c42bb23c9c0130a06b2c7a334d7f6d546e75e2
Chase Kim <chase79@gmail.com>
2024-07-30 09:02:33 -0700

    Third commit

--- main.js
+++ main.js
@@ -1,60 +1,63 @@
-
 let taskInput = document.getElementById("taskInput");
 let addButton = document.getElementById("addButton");
 let tabs = document.querySelectorAll(".taskTabs div");
 let taskList = [];
 let filteredList = [];
 let tabsID = "all";
-let list = ''
+let list = [];
 
-addButton.addEventListener("click", taskItem); 
+addButton.addEventListener("click", taskItem);
 
-// variable "tabs" has three items (tabs in div) in it by using querySelectorAll. 
-// Looping it to provide click event to each item in it.
-for (let i = 1; i < tabs.length; i++) { 
+// Receives three tabs info, under .taskTabs div, from tabs = document.querySelectorAll
+// For each tabs, gives click event with "filtering(event)"
+for (let i = 1; i < tabs.length; i++) {
     tabs[i].addEventListener("click", function(event) {
-        filtered(event);
+        filtering(event);
     });
-}   
-
-
-// Executes when addButton is clicked (+ button in UI).
-// First if-statement alerts when there is no user input value.
-// Each user input value gets added into an object "task" with randomly generated ID as well as "isComplete" key (this key has a default value of "false")
-// Each item in the object "task" gets appended to an array "taskList"
-function taskItem() {
-    if (taskInput.value.length < 1) {
-        return alert("Enter the item");
-    }
-    let task = {
-        id: randID(),
-        taskContent: taskInput.value,
-        isComplete: false
-    };
-    taskList.push(task);
-    render();
 }
 
 
-// This draws the UI on div id "taskBoard."
-// tabsID receives id for each tab from the event.target.
-// taskList contains all input values, filterList contains altered value (see filtered function at the near bottom section).
-function render() {
-    list = ''
-    if (tabsID == "all") {
+// called by addButton event listener
+// stores inputted value
+// the inputted value (in taskContent) gets appended to an array "taskList"
+function taskItem() {
+    if (taskInput.value ==false) {
+        return alert("hahaha");
+    }     
+    let task = {
+        id: randID(),
+        taskContent: taskInput.value,   
+        isComplete: false
+    };
+    taskList.push(task);   
+
+    render();                           
+}
+
+
+// updates UI on <div>taskBoard</div>
+// each inputted item were pushed to taskList.
+// taskList gets assigned to "list" based on condition in if statement.
+// if taskList.isComplete === false, displays input value on the UI as it is
+// if taskList.isComplete === true, draws the line-through over the input value on the UI
+function render() { 
+    list = [];
+    
+    if (tabsID === "all") {
         list = taskList;
-    } else if (tabsID == "onGoing" || tabsID == "done") {
+    } else if (tabsID === "onGoing" || tabsID=== "done") {
         list = filteredList;
     }
 
     let result = '';
-    for (let i = 0; i < list.length; i++) {
-        if (list[i].isComplete == false) {
+
+    for (let i = 0; i < list.length; i++) {         
+        if (list[i].isComplete === false) {
             result += `<div class="task">
                         <div>${list[i].taskContent}</div>
                         <div>
                             <button onclick="checkToggle('${list[i].id}')">Check</button>
-                            <button onclick ="deleteTask('${list[i].id}')">Delete</button>
+                            <button onclick="deleteTask('${list[i].id}')">Delete</button>
                         </div>
                     </div>`
         } else {
@@ -67,49 +70,47 @@ function render() {
                     </div>`
         }
     }
+    
     document.getElementById("taskBoard").innerHTML = result; 
 }
 
 
-// function for check button
-// Loops the array "taskList" to check if id (argument received) matches the tasklist.id
-// the argument id is received from onclick in render() (check result value > button onclick)
+// toggles between "input value with line-through" and "input value without line-through" when "Check" button is clicked
 function checkToggle(id) {
     for (let i = 0; i < taskList.length; i++) {
-        if (taskList[i].id == id) {
+        if (taskList[i].id === id) {
             taskList[i].isComplete =! taskList[i].isComplete;
         }
     }
+
     render();
 }
 
 
-// function for delete button
-// if argument received (randomly generated id) matches the list.id, it deletes it from the array. 
-// The item deletes from UI.
+// deletes the inputted value from both taskList and list and updates the UI
 function deleteTask(id) {
     for (let i = 0; i < list.length; i++) {
-        if (list[i].id == id) {
+        if (list[i].id === id) {
             list.splice(i, 1);
         }
     }
+
     render();
 }
 
 
-// function for tabs. Receives event from addEventListener.
-// alters the array "filterList" to be used from render().
-function filtered(event) {
-    tabsID = event.target.id;
+// Receives id info of the three tabs from function(event), and assigns to "tabsID"
+// Stores the item in "filteredList" based on its .isComplete value.
+function filtering(event) {
     filteredList = [];
-
+    tabsID = event.target.id;
     if (tabsID === "all") {
         render();
     } else if (tabsID === "onGoing") {
         for (let i = 0; i < taskList.length; i++) {
             if (taskList[i].isComplete === false) {
-                filteredList.push(taskList[i]); 
-            }// task.isComplete == false
+                filteredList.push(taskList[i]);
+            }
         }
         render();
     } else {
@@ -123,10 +124,9 @@ function filtered(event) {
 }
 
 
-// random ID generator for each user input value.
-// gets added to an object "task" in taskItem().
+// Randomly generated id
+// included as one of the value of the key in the variable "task" inside "taskItem()"
 function randID() {
     return '_' + Math.random().toString(36).substr(2, 9);
 }
-
 
